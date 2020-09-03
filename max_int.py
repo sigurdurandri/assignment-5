ea94ca2b27c1e4bcaa63475415c605f89d07ebbe
Sigurður Agnarsson <sigurdura19>
2020-09-03 14:47:27 +0000

    Jólasveinn

---
+++ max_int.py
@@ -0,0 +1,23 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+counter=1
+x=0
+y=0
+z=0
+summa=1
+while counter<n:
+    x= counter
+   
+
+    summa=x+y+z
+    print(summa, end=' ')
+    y=
+    x=
+    
+
+
+
+
+# Okey þetta forrit fer n oft í gegn og í hvert skipti tekur hann seinustu 
+#3 tölur og addar þeim saman
+
